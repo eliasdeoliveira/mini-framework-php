980b6d53b99c999bc45f2a5c9e45d2894a753c7a
Elias de Oliveira <eliasdeoliveirabr@gmail.com>
2022-09-23 11:37:55 -0300

    Ajuste base_url

--- app/config/config.php
+++ app/config/config.php
@@ -2,7 +2,7 @@
 
 define('BASE', '/');
 
-define('base_url', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME']);
+define('base_url', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST']);
 
 define('UNSET_URI_COUNT', 0);
 define('DEBUG_URI', false);
